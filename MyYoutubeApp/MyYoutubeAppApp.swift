d21b0ae7282d01d9513e41dc7141a435e2b3e0ef
Alperen Sarışan <88689564+althebeast@users.noreply.github.com>
2024-09-03 16:32:56 +0300

    Initial Commit

---
+++ MyYoutubeApp/MyYoutubeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyYoutubeAppApp.swift
+//  MyYoutubeApp
+//
+//  Created by Alperen Sarışan on 3.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct MyYoutubeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
